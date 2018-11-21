c70b1823f7c890468ba273d393f848c71f190b4b
elizabeth see <elliesee97@gmail.com>
2018-11-21 13:39:40 -0800

    work title animation

--- final/assets/script.js
+++ final/assets/script.js
@@ -1,32 +1,8 @@
 $(document).ready(function(){
-  //
-  // var ctx = canvas.getContext("2d"),
-  //     img = new Image,
-  //     radius = 40;
-  //
-  // img.onload = setup;
-  // img.src = "assets/img/Artboard 5-8.png";
-  //
-  // function setup() {
-  //
-  //   // set image as pattern for fillStyle
-  //   ctx.fillStyle = ctx.createPattern(this, "no-repeat");
-  //
-  //   // for demo only, reveals image while mousing over canvas
-  //   canvas.mousedown = function(e) {
-  //     var r = this.getBoundingClientRect(),
-  //         x = e.clientX - r.left,
-  //         y = e.clientY - r.top;
-  //
-  //     ctx.beginPath();
-  //     ctx.moveTo(x + radius, y);
-  //     ctx.arc(x, y, radius, 0, 2*Math.PI);
-  //     ctx.fill();
-  //   };
-  // }
 
-  $('#menu').click(function() {
-		$(this).toggleClass('menu2');
+
+  $('#sections1').hover(function() {
+		$(this).toggleClass('h31');
 	});
 
 
