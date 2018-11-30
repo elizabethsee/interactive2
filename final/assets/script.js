e5943b3252a0707c15bb0cb93d1921a66d324115
elizabeth see <elliesee97@gmail.com>
2018-11-29 16:02:25 -0800

    other work click additions

--- final/assets/script.js
+++ final/assets/script.js
@@ -19,6 +19,42 @@ $(document).ready(function(){
     // }
   });
 
+  var image =  document.getElementById("untitledshape1");
+
+
+  $('#untitledshape1').on('click', function() {
+
+    var n = Math.floor((Math.random() * 4) + 1);
+    $("#untitledshape1").attr('src', "assets/img/untitle/untitle"+n+".png");
+
+
+    // if (document.getElementById("curveshape1").src == "assets/img/curve/curve2.png") {
+    //   document.getElementById("curveshape1").src = "assets/img/curve/curve.png";
+    //   console.log('hello');
+    // } else {
+    //   document.getElementById("curveshape1").src = "assets/img/curve/curve2.png";
+    //   console.log('hi');
+    // }
+  });
+
+  var image =  document.getElementById("whiteshape1");
+
+
+  $('#whiteshape1').on('click', function() {
+
+    var n = Math.floor((Math.random() * 4) + 1);
+    $("#whiteshape1").attr('src', "assets/img/white/white"+n+".png");
+
+
+    // if (document.getElementById("curveshape1").src == "assets/img/curve/curve2.png") {
+    //   document.getElementById("curveshape1").src = "assets/img/curve/curve.png";
+    //   console.log('hello');
+    // } else {
+    //   document.getElementById("curveshape1").src = "assets/img/curve/curve2.png";
+    //   console.log('hi');
+    // }
+  });
+
 
   // function changeImage() {
   //
