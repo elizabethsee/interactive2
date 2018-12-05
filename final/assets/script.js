8a0a772daf1b69d44400deb7654ea559de481ca8
elizabeth see <elliesee97@gmail.com>
2018-12-05 00:01:45 -0800

    link upodate for homepage

--- final/assets/script.js
+++ final/assets/script.js
@@ -10,13 +10,7 @@ $(document).ready(function(){
     $("#curveshape1").attr('src', "assets/img/curve/curve"+n+".png");
 
 
-    // if (document.getElementById("curveshape1").src == "assets/img/curve/curve2.png") {
-    //   document.getElementById("curveshape1").src = "assets/img/curve/curve.png";
-    //   console.log('hello');
-    // } else {
-    //   document.getElementById("curveshape1").src = "assets/img/curve/curve2.png";
-    //   console.log('hi');
-    // }
+
   });
 
   var image =  document.getElementById("untitledshape1");
@@ -28,13 +22,6 @@ $(document).ready(function(){
     $("#untitledshape1").attr('src', "assets/img/untitle/untitle"+n+".png");
 
 
-    // if (document.getElementById("curveshape1").src == "assets/img/curve/curve2.png") {
-    //   document.getElementById("curveshape1").src = "assets/img/curve/curve.png";
-    //   console.log('hello');
-    // } else {
-    //   document.getElementById("curveshape1").src = "assets/img/curve/curve2.png";
-    //   console.log('hi');
-    // }
   });
 
   var image =  document.getElementById("whiteshape1");
@@ -46,25 +33,9 @@ $(document).ready(function(){
     $("#whiteshape1").attr('src', "assets/img/white/white"+n+".png");
 
 
-    // if (document.getElementById("curveshape1").src == "assets/img/curve/curve2.png") {
-    //   document.getElementById("curveshape1").src = "assets/img/curve/curve.png";
-    //   console.log('hello');
-    // } else {
-    //   document.getElementById("curveshape1").src = "assets/img/curve/curve2.png";
-    //   console.log('hi');
-    // }
+
   });
 
 
-  // function changeImage() {
-  //
-  //         if (document.getElementById("curveshape1").src == "assets/img/curve/curve.png")
-  //         {
-  //             document.getElementById("curveshape1").src = "assets/img/curve/curve2.png";
-  //         }
-  //         else
-  //         {
-  //             document.getElementById("curveshape1").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
-  //         }
-  //     }
+
 });
